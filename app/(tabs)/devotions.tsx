844e99bc9fae78357cf2e79dfce2407898151534
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-12-19 16:18:56 +0300

    chore: set up tab bar navigation stracture

---
+++ app/(tabs)/devotions.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Devotions = () => {
+  return (
+    <View>
+      <Text>Devotions</Text>
+    </View>
+  );
+};
+
+export default Devotions;
---
+++ app/(tabs)/gifts.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Gifts = () => {
+  return (
+    <View>
+      <Text>Gifts</Text>
+    </View>
+  );
+};
+
+export default Gifts;
