0a47e9dbd07773876afc15616c69d309dd03554f
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-12-24 01:46:45 +0300

    chore: update settings icon

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,24 @@
-import { Ionicons } from "@expo/vector-icons"; // Assuming you are using Expo, otherwise use your preferred icon lib
+import { Ionicons } from "@expo/vector-icons"; // Expo icons
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const Home = () => {
   return (
-    <View className="flex-1 bg-gray-50">
+    <View className="flex-1 bg-white">
       {/* HEADER */}
-      <View className="flex-row justify-between items-center px-6 pt-6 pb-1 bg-white shadow-sm">
-        <View className="flex-row items-center">
-          <Image
-            source={require("../../assets/images/header.png")}
-            className="w-20 h-20"
-            resizeMode="contain"
-          />
-        </View>
+      <View className="flex-row justify-between items-center px-6 pt-6 pb-2 h-28 bg-white shadow-sm">
+        <Image
+          source={require("../../assets/images/header.png")}
+          className="w-24 h-24"
+          resizeMode="contain"
+        />
 
         <TouchableOpacity>
-          <Ionicons name="settings-outline" size={28} color="#1e293b" />
+          <Ionicons name="person-circle-outline" size={36} color="#1e293b" />
         </TouchableOpacity>
       </View>
 
-      {/* Body*/}
+      {/* BODY */}
       <View className="flex-1 items-center justify-center">
         <Text className="text-gray-400">Content</Text>
       </View>
