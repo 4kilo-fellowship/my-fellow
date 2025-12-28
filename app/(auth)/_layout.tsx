d2e4d85d4de3ee23279d2cc51d6ce5370ea6b6ed
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-12-28 22:39:08 +0300

    chore: set up stack navigation for auth screen

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,17 @@
+import { Stack } from "expo-router";
+
+export default function AuthLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: "#F9FAFB" },
+        animation: "slide_from_right",
+      }}
+    >
+      <Stack.Screen name="sign-in" />
+      <Stack.Screen name="sign-up-step-1" />
+      <Stack.Screen name="sign-up-step-2" />
+    </Stack>
+  );
+}
