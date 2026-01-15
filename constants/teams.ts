dfbf528415a0199b9508e1d3a04954da58df7917
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-15 09:36:57 +0300

    chore: set up teams constants

---
+++ constants/teams.ts
@@ -0,0 +1,10 @@
+export const TEAMS = [
+  "Evangelism Team",
+  "Bible Study Team",
+  "Worship Team",
+  "I4U Team",
+  "Prayer Team",
+  "Literature and Media Team",
+  "No Team",
+  "Other",
+] as const;
