be60c4ab5005719ce233669d2527055154afeb44
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-15 09:43:14 +0300

    chore: set up departments constants

---
+++ constants/departments.ts
@@ -0,0 +1,11 @@
+export const DEPARTMENTS = [
+  "Applied Chemistry",
+  "Applied Mathematics",
+  "Applied Biology",
+  "Computer Science",
+  "Statistics",
+  "Geology",
+  "Medicine",
+  "Business & Econ",
+  "Other",
+] as const;
