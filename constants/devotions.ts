d38df607092c709ffbe88dad00112d8b2e297763
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-20 09:23:55 +0300

    chore: add more dummy data

--- constants/devotions.ts
+++ constants/devotions.ts
@@ -25,4 +25,40 @@ export const DEVOTIONS: Devotion[] = [
     image:
       "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
   },
+  {
+    id: "3",
+    title: "Daily Gratitude",
+    date: "Jan 08",
+    views: "1.1k",
+    likes: "295",
+    image:
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+  },
+  {
+    id: "4",
+    title: "Evening Reflection",
+    date: "Jan 06",
+    views: "850",
+    likes: "180",
+    image:
+      "https://images.unsplash.com/photo-1508780709619-79562169bc64?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+  },
+  {
+    id: "5",
+    title: "Faith and Hope",
+    date: "Jan 04",
+    views: "1.5k",
+    likes: "400",
+    image:
+      "https://images.unsplash.com/photo-1473187983305-f615310e7daa?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+  },
+  {
+    id: "6",
+    title: "Spiritual Growth",
+    date: "Jan 02",
+    views: "1.0k",
+    likes: "250",
+    image:
+      "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+  },
 ];
