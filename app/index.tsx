30749e2cd467afa4d79c3edf1d427bce9307fa95
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-30 13:14:03 +0300

    fix: resolve colors path issue

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { PRIMARY } from "@/constants/colors";
+import { PRIMARY } from "@/constants";
 import { Image } from "expo-image";
 import { useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
