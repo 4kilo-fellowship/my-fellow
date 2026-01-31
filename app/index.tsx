0b957591d6d34242c37b7cf73c78b995f79d4217
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-01-31 06:37:30 +0300

    chore: improve the splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,49 @@
-import { Redirect } from "expo-router";
+import { PRIMARY } from "@/constants";
+import { Image } from "expo-image";
+import { useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
 
-export default function Index() {
-  return <Redirect href="/(tabs)" />;
+export default function SplashScreen() {
+  const router = useRouter();
+  const [phase, setPhase] = useState<"primary" | "white">("primary");
+
+  useEffect(() => {
+    const changeTimeout = setTimeout(() => {
+      setPhase("white");
+    }, 3000);
+
+    const navTimeout = setTimeout(() => {
+      // After the splash screen, navigate into the auth flow
+      router.replace("/(tabs)");
+    }, 4000);
+
+    return () => {
+      clearTimeout(changeTimeout);
+      clearTimeout(navTimeout);
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: phase === "primary" ? PRIMARY : "#fff",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image
+        source={
+          phase === "primary"
+            ? require("../assets/images/logo-white.png")
+            : require("../assets/images/logo-primary.png")
+        }
+        style={{ width: 500, height: 500 }}
+        contentFit="contain"
+        transition={0}
+        cachePolicy="memory"
+      />
+    </View>
+  );
 }
